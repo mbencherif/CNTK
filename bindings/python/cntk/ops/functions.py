d95840bd585fd724cd23c81b0302aeadd3b4877a
Philipp Kranen <pkranen@microsoft.com>
2017-03-22 17:47:05 +0100

    fwd and backward working for Faster RCNN

--- bindings/python/cntk/ops/functions.py
+++ bindings/python/cntk/ops/functions.py
@@ -1199,9 +1199,10 @@ class UserFunction(Function):
                 break
             root_gradients = rg
 
-        possible_wrt = [input for input in self.inputs if input.needs_gradient]
-        print ("{}: nr of needs_gradient: {}".format(self.name, len(possible_wrt)))
-        if len(possible_wrt) > 1:
+        #possible_wrt = [input for input in self.inputs if input.needs_gradient]
+        #print ("{}: nr of variables: {}".format(self.name, len(variables)))
+        #import pdb; pdb.set_trace()
+        if True: #len(variables) > 1:
             self.backward(state, root_gradients, variables)
         else:
             result = self.backward(state, root_gradients)
